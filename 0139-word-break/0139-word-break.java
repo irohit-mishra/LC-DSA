72dffe35ccb9cb71542ca0e6c25b35b442bed981
Rohit Mishra <mail.irohitmishra@gmail.com>
2024-07-07 20:17:00 +0530

    Time: 2 ms (88.72%), Space: 41.4 MB (95.19%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,24 +1,31 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-      //convert wordDict to  hashset for the false look-up
-      Set<String> wordSet = new HashSet<>(wordDict);
-
-      //dp array to store the segmentation status
-      boolean[] dp = new boolean[s.length() + 1];
-
-      //base case 
-      dp[0] = true;
-
-      //fill the DP array
-      for (int i = 1; i <= s.length(); i++) {
-        for (int j=0; j < i; j++){
-            if(dp[j] && wordSet.contains(s.substring(j, i))){
-                dp[i]= true;
-                break;
+        // Convert wordDict to a HashSet for fast look-up
+        Set<String> wordSet = new HashSet<>(wordDict);
+        
+        // Set to store lengths of words in the dictionary
+        Set<Integer> wordLengths = new HashSet<>();
+        for (String word : wordDict) {
+            wordLengths.add(word.length());
+        }
+        
+        // DP array to store the segmentation status
+        boolean[] dp = new boolean[s.length() + 1];
+        
+        // Base case: empty string
+        dp[0] = true;
+        
+        // Fill the DP array
+        for (int i = 1; i <= s.length(); i++) {
+            for (int length : wordLengths) {
+                if (i >= length && dp[i - length] && wordSet.contains(s.substring(i - length, i))) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-      }  
-      //the result is whether the entire string s can be segmented
-      return dp[s.length()]; 
+        
+        // The result is whether the entire string s can be segmented
+        return dp[s.length()];
     }
 }
