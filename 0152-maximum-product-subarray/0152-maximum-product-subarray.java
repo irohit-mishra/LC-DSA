6eb9a7f5b1e48343867709716654cb69537ee543
Rohit Mishra <mail.irohitmishra@gmail.com>
2024-07-20 11:17:36 +0530

    Time: 2 ms (73.38%), Space: 44.7 MB (62.67%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // Since we're dealing with negative numbers, we need to consider both the maximum
+            // and minimum products so far, as multiplying a negative number can flip the sign.
+            if (nums[i] < 0) {
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
+            }
+
+            maxProduct = Math.max(nums[i], maxProduct * nums[i]);
+            minProduct = Math.min(nums[i], minProduct * nums[i]);
+
+            
+            result = Math.max(result, maxProduct);
+        }
+
+        return result;
+    }
+}
