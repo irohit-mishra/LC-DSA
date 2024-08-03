2707c6d97df0409b5b4765c84139c5286141e73a
Rohit Mishra <mail.irohitmishra@gmail.com>
2024-08-03 22:32:31 +0530

    Time: 9 ms (92.22%), Space: 56.9 MB (35.43%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        // Base case: if the list is empty or has only one node
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Step 1: Split the list into two halves
+        ListNode mid = getMid(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+
+        // Step 2: Sort each half
+        left = sortList(left);
+        right = sortList(right);
+
+        // Step 3: Merge the sorted halves
+        return merge(left, right);
+    }
+
+    // Function to find the middle of the linked list
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // Function to merge two sorted linked lists
+    private ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (list1 != null) {
+            tail.next = list1;
+        } else {
+            tail.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
